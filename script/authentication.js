7f5f1db5141783060831dc8a6832479c57d81596
Schr0Sm1ley <radiantremel444@gmail.com>
2024-09-23 01:34:35 +0600

    Fixed the path

--- script/authentication.js
+++ script/authentication.js
@@ -5,16 +5,14 @@ function togglePassword(event, id_pass, id_icon) {
 
     if (passwordField.type == 'password') {
         passwordField.type = 'text';
-        toggleIcon.src = '/Project/PaySwift/images/hide.png';
-        // toggleIcon.src = '../images/hide.png';
+        toggleIcon.src = '../images/hide.png';
         toggleIcon.alt = 'Hide';
         toggleIcon.style.height = '20px';
         toggleIcon.style.width = '20px';
     }
     else {
         passwordField.type = 'password';
-        toggleIcon.src = '/Project/PaySwift/images/show.png';
-        // toggleIcon.src = '../images/show.png';
+        toggleIcon.src = '../images/show.png';
         toggleIcon.alt = 'Show';
         toggleIcon.style.height = '20px';
         toggleIcon.style.width = '20px';
